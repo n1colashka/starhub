168a2d730529490903cdab5672c7479510aec6cf
Nick <deckards2505@gmail.com>
2020-08-29 12:24:47 +0600

    Menu fixed.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.header').addEventListener('click', function(evt) {
         if (evt.target.closest('.menu__btn')) {
-            document.querySelector('.menu').classList.toggle('menu--active');
+            document.querySelector('.menu').classList.add('menu--active');
         }
+
+        if (evt.target.closest('.menu__services-close')) {
+            document.querySelector('.menu').classList.remove('menu--active');
+        }
+        
     });
 });
